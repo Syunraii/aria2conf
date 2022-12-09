1b4593699ab7857b9f7b56359544c08e1ef8870b
Syunraii <Syunraii@outlook.com>
2022-12-10 03:56:27 +0800

    fix err

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-#trackers-list-aria2.sh
 
-# aria2 设置文件路径 改为自己的路径 
+# aria2 设置文件路径
 CONF=${HOME}/.aria2/aria2.conf
 
-# 推荐 大佬整合的下各类资源
+
+#设置选择的 trackerlist （可选 all_aria2.txt, best_aria2.txt, http_aria2.txt）
+trackerfile=all_aria2.txt
+downloadfile=https://trackerslist.com/${trackerfile}
+
+# 推荐 大佬整合的下各类资源 需要自己写正则匹配
 ## https://github.com/ngosang/trackerslist
-trackers1 = trackers_best.txt
-# trackers1 = tracker_all.txt
-# trackers1 = tracker_udp.txt
-# trackers1 = tracker_http.txt
-# trackers1 = tracker_https.txt
-#downloadfile=https://raw.githubusercontent.com/ngosang/trackerslist/master/${}
+# trackers1=trackers_best.txt
+# trackers1=tracker_all.txt
+# trackers1=tracker_udp.txt
+# trackers1=tracker_http.txt
+# trackers1=tracker_https.txt
+# downloadfile=https://raw.githubusercontent.com/ngosang/trackerslist/master/${trackers1}
 
 ## https://github.com/XIU2/TrackersListCollection
-trackers2 = best.txt
-# trackers2 = all.txt
-# trackers2 = http.txt
+#trackers2=best.txt
+# trackers2=all.txt
+# trackers2=http.txt
 # downloadfile=https://jsd.cdn.zzko.cn/gh/XIU2/TrackersListCollection/${trackers2}
 
-# 主要下载动漫
 # https://github.com/DeSireFire/animeTrackerList
-tracker3 = AT_best.txt
-# tracker3 = AT_all.txt
-# tracker3 = AT_all_ip.txt
-# tracker3 = AT_all_udp.txt
-# tracker3 = AT_all_http.txt
-# tracker3 = AT_all_https.txt
-downloadfile=https://github.com/DeSireFire/animeTrackerList/blob/master/${tracker3}
+# tracker3=AT_best.txt
+# tracker3=ATaria2_all.txt
+# tracker3=ATaria2_all_udp.txt
+# tracker3=ATaria2_all_http.txt
+# tracker3=ATaria2_all_https.txt
+# tracker3=ATaria2_all_ws.txt
+# tracker3=ATaria2_best_ip.txt
+# tracker3=ATaria2_all_ip.txt
+# downloadfile=https://cdn.jsdelivr.net/gh/DeSireFire/animeTrackerList/${tracker}
 
 
+echo -e "链接:${downloadfile}"
 list=$(curl -fsSL ${downloadfile})
 if ! grep -q "bt-tracker" "${CONF}" ; then
     echo -e "\033[34m==> 添加 bt-tracker 服务器信息......\033[0m"
@@ -37,6 +43,7 @@ if ! grep -q "bt-tracker" "${CONF}" ; then
 else
     echo -e "\033[34m==> 更新 bt-tracker 服务器信息.....\033[0m"
     sed -i '' "s@bt-tracker.*@bt-tracker=${list}@g" "${CONF}"
+    echo -e "更新完毕"
 fi
 
 ## 重启 aria2 服务
